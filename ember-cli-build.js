d696a048b73d94b7dfdfc02d02dad3cfb1b658f4
Kamilla <holanda.kamilla@gmail.com>
2016-06-10 18:18:56 -0300

    Adding assets

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,3 @@
-/*jshint node:true*/
-/* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
@@ -19,6 +17,11 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('vendor/fontello/fontello.css');
+  
+
+  app.import('vendor/basscss.min.css');
 
   return app.toTree();
 };
